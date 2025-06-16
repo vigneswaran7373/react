2cb1d86707ad66205c1388a7223aeb000b3f0bbf
vigneswaran7373 <vigneswaran7373@gmail.com>
2025-06-16 13:11:35 +0530

    Add files via upload

---
+++ basicformreact.js
@@ -0,0 +1,14 @@
+function Basicformreact(){
+    const mystyle={
+        color:"red",
+        backgroundColor:"black",
+    }
+    return(
+   <>
+ 
+   <h1 className="col">hello hi </h1>
+   <h2 style={mystyle}>ahh good</h2>
+   </>
+    );
+}
+export default Basicformreact;
